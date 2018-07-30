a9762da4716b43f34ce32d29cadcc35d4e4d6649
Your Name <you@example.com>
2018-07-30 08:40:02 +0800

    simulate new method

---
+++ src/LotsOfEntriesMain.java
@@ -0,0 +1,24 @@
+// import net.openhft.chronicle.map.*;
+//
+// interface PostalCodeRange {
+// int minCode();
+//
+// void minCode(int minCode);
+//
+// int maxCode();
+//
+// void maxCode(int maxCode);
+// }
+//
+// public class LostsOfEntriesMain(){
+// public static class Main()
+// }
+//
+//
+// ChronicleMapBuilder<CharSequence, PostalCodeRange> cityPostalCodesMapBuilder
+// = ChronicleMapBuilder
+// .of(CharSequence.class,
+// PostalCodeRange.class).name("city-postal-codes-map").averageKey("Amsterdam")
+// .entries(50_000);
+// ChronicleMap<CharSequence, PostalCodeRange> cityPostalCodes =
+// cityPostalCodesMapBuilder.create();
--- src/SimulateLessObjects.java
+++ src/SimulateLessObjects.java
@@ -1,10 +1,12 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Main {
+public class SimulateLessObjects {
 	static Map<String, HashMap<Integer, List<Object>>> rateMap = new HashMap<String, HashMap<Integer, List<Object>>>();
 	static String rateMapKey = null;
 
@@ -14,13 +16,75 @@ public class Main {
 	static String effectiveFrom = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 	static String effectiveTo = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
 
+	static HashMap<String, String> hMap2 = new HashMap<>();
+
+	// create a sample column value
+	public static Double colValue() {
+		Double sampleRate = ThreadLocalRandom.current().nextDouble(0, 20000);
+		sampleRate = round(sampleRate, 2);
+		// System.out.println(sampleRate);
+
+		return sampleRate;
+	}
+
+	public static String fullString() {
+
+		// create a 99 column long string, delimited by comma;
+		String fullString = "";
+
+		for (int i = 0; i < 100; i++) {
+			fullString = fullString + Double.toString(colValue()) + ",";
+		}
+
+		// remove the last comma
+		fullString = fullString.substring(0, fullString.length() - 1);
+
+		return fullString;
+
+	}
+
 	public static void main(String[] args) {
 
-		// Connecting to Redis server on localhost
-		// Jedis jedis = new Jedis("localhost");
-		System.out.println("Connection to server sucessfully");
-		// check whether server is running or not
-		// System.out.println("Server is running: " + jedis.ping());
+		// System.out.println(fullString);
+
+		// simulate one sheet - 5000 lines and 99 columns
+
+		// key = payment term + policy year + Sex, Smoker, Unique Identifier,
+		// Effective From, Effective To
+		// key - value, key = 10_5_M_N_(UI)_198764556544_177990980809
+
+		String key = null;
+		String simFullString = "";
+		for (int i = 0; i < 500000000; i++) {
+
+			key = "10_5_M_N_";
+
+			key = key + String.valueOf(i) + "_198764556544_177990980809";
+			simFullString = fullString();
+
+			// System.out.println("key is " + key + ", index = " +
+			// String.valueOf(i));
+			// System.out.println("value is " + simFullString);
+
+			hMap2.put(key, simFullString);
+
+			if (i % 50000 == 0) {
+				System.out.println("10 pages mate, well done!");
+			}
+		}
+
+	}
+
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+
+		BigDecimal bd = new BigDecimal(value);
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd.doubleValue();
+	}
+
+	public static void main2(String[] args) {
 
 		for (int i = 0; i < 1000000000; i++) {
 
